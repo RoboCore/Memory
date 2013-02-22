9c9b56c3806f9a6d90c9e092e05c61e43cc268e8
gabriel <gabriel@robocore.net>
2013-02-22 17:11:11 -0300

    Changed README.txt.

--- Memory.h
+++ Memory.h
@@ -5,7 +5,7 @@
 	RoboCore Memory Library
 		(v1.0 - 22/02/2013)
 
-  Library to get the status of the memory (for Arduino 1.0 or later)
+  Memory functions for Arduino (for Arduino 1.0 or later)
 
   Released under the Beerware licence
 */
